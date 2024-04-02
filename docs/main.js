2eda9a88d40bb5392355fe6795d944b8181f4c4f
Nathan Studley <nathanstudley@gmail.com>
2024-04-02 17:23:01 -0400

    home page edts

--- docs/main.js
+++ docs/main.js
@@ -1,24 +1,11 @@
-const pages = document.getElementsByClassName('page');
-const homePage = document.getElementById('home');
-const contactPage = document.getElementById('contact');
-const resumePage = document.getElementById('resume');
-const projectsPage = document.getElementById('projects');
+const maincontent = document.getElementById('maincontent');
 const menuButton = document.getElementById('menuButton');
 const navMenu = document.getElementById('navmenu');
-
-function goToPage(page) {
-    for (let i = 0; i < pages.length; i++){
-        pages[i].style.display = 'none';
-    }
-    page.style.display = 'block';
-}
-
-const goHome = () => goToPage(homePage);
-const goResume = () => goToPage(resumePage);
-const goContact = () => goToPage(contactPage);
-const goProjects = () => goToPage(projectsPage);
 
 menuButton.addEventListener('click', () => {
     navMenu.classList.toggle('hidden');
 });
+
+
+
 
