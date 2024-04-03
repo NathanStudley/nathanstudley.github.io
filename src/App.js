62f79fc63ff724bcc7d531d038f014b0f6d221ca
Nathan Studley <nathanstudley@gmail.com>
2024-04-03 13:03:33 -0400

    react setup with tailwind

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10'>
+      <h1 className='text-5xl'>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
