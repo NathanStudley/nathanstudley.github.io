db28ef654762c691f7fa73376a94621c8d3d64b1
Nathan Studley <nathanstudley@gmail.com>
2024-04-06 09:46:50 -0400

    add header to react app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Components/Header";
 
 function App() {
   return (
-    <div className='container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10'>
-      <h1 className='text-5xl'>Hello World</h1>
-    </div>
+    <Header />
   );
 }
 
